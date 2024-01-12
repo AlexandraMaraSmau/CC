8f7ab95fbc69fcdb1e9cf3685b413906b008f8ec
Ledezeu <ledezeurobert.andrei@yahoo.com>
2024-01-12 21:46:05 +0200

    added sql database

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@ CREATE TABLE User (
                       LastName VARCHAR(255),
                       FirstName VARCHAR(255),
                       Role VARCHAR(255),
-                      Email VARCHAR(255),
+                      Email VARCHAR(255) UNIQUE,
                       Password VARCHAR(255),
                       PhoneNumber VARCHAR(15),
                       ProfilePicture VARCHAR(255),
